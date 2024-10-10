fd8b00448143f64cde242892a59b8047fcc2df95
Alvaro Escarti <escartismx@gmail.com>
2024-10-10 23:02:42 +0200

    Ejercicios

---
+++ DWES/UD-3/ejercicio13.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @Author: Álvaro Escartí
+ */
+
+
+
+ /**
+  * @Author: Álvaro Escartí
+  */
+ 
+ function elevarNumeros($numero1, $numero2){
+     $resultado = pow($numero1, $numero2); // Eleva el número1 a la potencia de número2
+     return $resultado;
+ }
+ 
+ echo "El resultado es: " . elevarNumeros(4,5) . "\n";
+
+ 
+?>
